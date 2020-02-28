3dfe807db1b1d305123bed7139d204cba8586bbc
ahusanov <a.khusanov@gmail.com>
2020-02-27 22:58:11 -0500

    login page with POM added

--- features/ui/tests/test_wd_advanced.py
+++ features/ui/tests/test_wd_advanced.py
@@ -1,22 +1,5 @@
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import selenium1.utilities as utils
-from selenium.common.exceptions import NoSuchElementException
-import pytest
+from features.ui.all_imports import *
 
-
-# AGENDA: 
-    # methods for performing keyboard and mouse actions using ActionChains class
-    # simulating mouse operations such as draand driop and double click
-    # Running JacaScript code
-    # capturing screenshots and movies of test runs
-
-print(utils.get_timestamp())
 
 @pytest.mark.screenshots
 def test_take_screenshots(browser):
@@ -25,34 +8,28 @@ def test_take_screenshots(browser):
     url = "http://the-internet.herokuapp.com/login"
     browser.get(url)
 
-    # Steps to automate:
-    # use existing scipt
-    # use try except 
-    # get timestamp
-    # save screenshot with driver.save_screenshot(file) , use this path "./screenshots/timestamp.png"
-    # log each step with print
-
-
     try:
         # use the following Login steps we created previously
         print("loggin page started..")
-        username = browser.find_element_by_xpath("//input[@id='username']")
-        passwrod = browser.find_element_by_xpath("//input[@id='password']")
-        login = browser.find_element_by_xpath("//i[@class='fa fa-2x fa-sign-in']")
-        username.send_keys("tomsmith")
-        passwrod.send_keys("SuperSecretPassword!")
-        login.click()
+        login_page = Login(browser)
+        login_page.enter_username("tomsmith")
+        login_page.enter_password("SuperSecretPassword!")
+        login_page.click_login()
+        
         print("logged in, taking screenshot")
         sleep(10)
-        filepath = "./screenshots/"+ utils.get_timestamp() +".png"
+        # assert "titlename" == login_page.get_title
+
+        filepath = "./screenshots/" + utils.get_timestamp() + ".png"
         browser.save_screenshot(filepath)
         print('test completed!')
 
     except NoSuchElementException:
         print("Something went wrong!")
-        filepath = "./screenshots/error-"+ utils.get_timestamp() +".png"
+        filepath = "./screenshots/error-" + utils.get_timestamp() + ".png"
         browser.save_screenshot(filepath)
         raise
+
 
 @pytest.mark.popupwindow
 def test_popup_window(browser):
@@ -80,17 +57,19 @@ def test_popup_window(browser):
         if handle != parentHandle:
             browser.switch_to.window(handle)
             print('switching to new window')
-            search = browser.find_element_by_xpath("//input[@id='search-courses']")
+            search = browser.find_element_by_xpath(
+                "//input[@id='search-courses']")
             search.send_keys("python")
             search.submit()
             sleep(5)
             print('search successfully executed, taking screenshot')
-            filepath = "./screenshots/window-"+ utils.get_timestamp() +".png"
+            filepath = "./screenshots/window-" + utils.get_timestamp() + ".png"
             browser.save_screenshot(filepath)
-    
+
     browser.switch_to.window(parentHandle)
     print('switched back to original window')
     sleep(5)
+
 
 @pytest.mark.executejs
 def test_execute_js(browser):
@@ -98,7 +77,8 @@ def test_execute_js(browser):
 
     url = "https://learn.letskodeit.com/p/practice"
     browser.get(url)
-    browser.execute_script("window.location = 'https://learn.letskodeit.com/p/practice';")
+    browser.execute_script(
+        "window.location = 'https://learn.letskodeit.com/p/practice';")
 
     # element = browser.find_element_by_id('name')
     element = browser.execute_script("return document.getElementById('name');")
@@ -111,6 +91,7 @@ def test_execute_js(browser):
     # Scroll up
     browser.execute_script("window.scrollBy(0, -1300);")
     sleep(5)
+
 
 @pytest.mark.hoveraction
 def test_hover_action(browser):
@@ -128,13 +109,14 @@ def test_hover_action(browser):
     browser.execute_script("window.scrollBy(0, 1000);")
     sleep(5)
     element = browser.find_element_by_xpath("//button[@id='mousehover']")
-    actions =  ActionChains(browser)
-        # Sample action from ActionChains
-            # actions.key_down(Keys.CONTROL)
-            # actions.send_keys('c')
-            # actions.key_up(Keys.CONTROL).perform()
+    actions = ActionChains(browser)
+    # Sample action from ActionChains
+    # actions.key_down(Keys.CONTROL)
+    # actions.send_keys('c')
+    # actions.key_up(Keys.CONTROL).perform()
     actions.move_to_element(element).perform()
-    topButton = browser.find_element_by_xpath("//a[contains(text(),'Top')]")  # "//a[text()='Top']"
+    topButton = browser.find_element_by_xpath(
+        "//a[contains(text(),'Top')]")  # "//a[text()='Top']"
     sleep(5)
     topButton.click()
     sleep(10)
@@ -154,7 +136,7 @@ def test_drag_and_drop(browser):
     # locate droppable element
     # create an object of ActionChains(driver) class
     # perform drag_and_drop action
-        # Alternative: perform click_and_hold(element1).move_to_element(element2).release action
+    # Alternative: perform click_and_hold(element1).move_to_element(element2).release action
     # log each step with print
     print("locating the both elements")
     sleep(3)
@@ -166,7 +148,7 @@ def test_drag_and_drop(browser):
 
     # element1 = browser.find_element_by_css_selector('#draggable')
     # element2 = browser.find_element_by_css_selector('#droppable')
-    
+
     print('creationg actions and performing drag and drop')
     actions = ActionChains(browser)
     sleep(3)
