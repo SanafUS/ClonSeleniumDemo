cc6eb5f302bf6fb635f1ee1d777685aa506e2f6a
ahusanov <a.khusanov@gmail.com>
2020-02-29 04:13:05 -0500

    Task: directory nessary files created

--- features/ui/step_definitions/utilities.py
+++ features/ui/step_definitions/utilities.py
@@ -1,8 +1,30 @@
 import time
+import yaml
 
 
 def get_timestamp():
     return time.strftime("%Y%m%d-%H%M%S")
 
 
-# print(get_timestamp())
+def create_logger(file=""):
+    """creates log file with time stamp filename under log directory"""
+    filepath = time.strftime("%Y%m%d")
+    filepath = f".//logs//{file}{filepath}.log"
+    logging.basicConfig(filename=filepath,
+                        level=logging.INFO,
+                        format='%(levelname)s %(asctime)s - %(message)s',
+                        filemode='w')
+    return logging.getLogger()
+
+
+def yaml_loader(filepath):
+    """Loads a yaml file"""
+    with open(filepath, 'r') as file_descriptor:
+        data = yaml.load(file_descriptor)
+    return data
+
+
+def yaml_dump(filepath, data):
+    """Dumps data to a yaml file"""
+    with open(filepath, 'w') as file_descriptor:
+        yaml.dump(data, file_descriptor)
